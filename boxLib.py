8d8154084e04152c8e3368446d794d4353b7637d
martinbros <67989270+martinbros@users.noreply.github.com>
2023-02-20 22:49:10 -0800

    Formatting + functionality
    Formatted some lines, added command line functionality

--- boxLib.py
+++ boxLib.py
@@ -2,14 +2,15 @@ import numpy as np
 import ezdxf
 import matplotlib.pyplot as plt
 
-#https://www.euclideanspace.com/maths/geometry/affine/aroundPoint/matrix2d/
-#https://ezdxf.readthedocs.io/en/stable/tutorials/lwpolyline.html
+# https://www.euclideanspace.com/maths/geometry/affine/aroundPoint/matrix2d/
+# https://ezdxf.readthedocs.io/en/stable/tutorials/lwpolyline.html
+
 
 def graphPoints(pointsList, color="k"):
-    
-    for idx in range(len(pointsList)-1):
-        #print("%s : %s" % (pointsList[idx], pointsList[idx + 1]))
+
+    for idx in range(len(pointsList) - 1):
         plt.plot([pointsList[idx][0], pointsList[idx + 1][0]], [pointsList[idx][1], pointsList[idx + 1][1]], color=color)
+
 
 def dogBoneCheck(boneDia, numFingers, clearence, span):
     polarity = numFingers / np.absolute(numFingers)
@@ -22,6 +23,7 @@ def dogBoneCheck(boneDia, numFingers, clearence, span):
         unit = (span - (2.0 * numFingers * clearence)) / (2.0 * numFingers + 1.0)
 
     return numFingers * polarity
+
 
 def genPoints(origin, typeSide, numFingers, fingerLength, clearence, dogDia, bulge, span, extra):
 
@@ -77,7 +79,8 @@ def genPoints(origin, typeSide, numFingers, fingerLength, clearence, dogDia, bul
 
     return(np.dstack((xList, yList, bulgeList))[0], unit)
 
-def rotate_points(points, angle, origin=(0,0)):
+
+def rotate_points(points, angle, origin=(0, 0)):
     # convert angle to radians
     angle_rad = np.radians(angle)
     # create the translation matrix to move the origin to the specified point
@@ -95,76 +98,79 @@ def rotate_points(points, angle, origin=(0,0)):
     # concatenate the matrices to create the transformation matrix
     transformation_matrix = translation_matrix @ rotation_matrix @ inverse_translation_matrix
     # create a matrix of column vectors from the points
-    points_matrix = np.hstack([points[:,:2], np.ones((len(points), 1))]).T
+    points_matrix = np.hstack([points[:, :2], np.ones((len(points), 1))]).T
     # apply the transformation to the points
     transformed_points = transformation_matrix @ points_matrix
 
     # Apply Bulge List back in
-    transformed_points[-1] = points[:,-1]
+    transformed_points[-1] = points[:, -1]
     # return the transformed points as a 2D numpy array
     return transformed_points.T
 
+
 def gen_bottom(orgin, numFingList, fingerLength, clearence, spanList, dogBoneDiameter, boneBulge):
-	numFingList[0] = dogBoneCheck(dogBoneDiameter, numFingList[0], clearence, spanList[0])
-	numFingList[1] = dogBoneCheck(dogBoneDiameter, numFingList[1], clearence, spanList[1])
+    numFingList[0] = dogBoneCheck(dogBoneDiameter, numFingList[0], clearence, spanList[0])
+    numFingList[1] = dogBoneCheck(dogBoneDiameter, numFingList[1], clearence, spanList[1])
 
-	allPoints, yUnit = genPoints(orgin, typeSide="thin", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
-	allPoints = rotate_points(allPoints, 90, orgin)
+    allPoints, yUnit = genPoints(orgin, typeSide="thin", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
+    allPoints = rotate_points(allPoints, 90, orgin)
 
-	points, xUnit = genPoints(allPoints[-1], typeSide="thin", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
-	rightPoint = points[-1]
-	allPoints = np.concatenate([allPoints, points])
+    points, xUnit = genPoints(allPoints[-1], typeSide="thin", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
+    rightPoint = points[-1]
+    allPoints = np.concatenate([allPoints, points])
 
-	points, unit = genPoints(points[-1], typeSide="thin", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
-	points = rotate_points(points, 270, points[0])
-	allPoints = np.concatenate([allPoints, points])
+    points, unit = genPoints(points[-1], typeSide="thin", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
+    points = rotate_points(points, 270, points[0])
+    allPoints = np.concatenate([allPoints, points])
 
-	points, unit = genPoints(points[-1], typeSide="thin", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
-	points = rotate_points(points, 180, points[0])
-	allPoints = np.concatenate([allPoints, points])
+    points, unit = genPoints(points[-1], typeSide="thin", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=0.0, dogDia=dogBoneDiameter, bulge=boneBulge)
+    points = rotate_points(points, 180, points[0])
+    allPoints = np.concatenate([allPoints, points])
 
-	return(allPoints, [orgin, rightPoint], [xUnit, yUnit])
+    return(allPoints, [orgin, rightPoint], [xUnit, yUnit])
+
 
 def gen_Side(orgin, rotation, numFingList, fingerLength, clearence, spanList, extraList, dogBoneDiameter, boneBulge):
-	numFingList[0] = dogBoneCheck(dogBoneDiameter, numFingList[0], clearence, spanList[0])
-	numFingList[1] = dogBoneCheck(dogBoneDiameter, numFingList[1], clearence, spanList[1])
+    numFingList[0] = dogBoneCheck(dogBoneDiameter, numFingList[0], clearence, spanList[0])
+    numFingList[1] = dogBoneCheck(dogBoneDiameter, numFingList[1], clearence, spanList[1])
 
-	polarity = numFingList[1] / np.absolute(numFingList[1])
+    polarity = numFingList[1] / np.absolute(numFingList[1])
 
-	allPoints, xUnit = genPoints(orgin, typeSide="thick", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=extraList[0], dogDia=dogBoneDiameter, bulge=boneBulge)
+    allPoints, xUnit = genPoints(orgin, typeSide="thick", numFingers=numFingList[0], fingerLength=fingerLength, clearence=clearence, span=spanList[0], extra=extraList[0], dogDia=dogBoneDiameter, bulge=boneBulge)
 
-	if polarity > 0:
-		points, yUnit = genPoints(allPoints[-1], typeSide="thin", numFingers=numFingList[1], fingerLength=-fingerLength, clearence=clearence, span=spanList[1], extra=extraList[1], dogDia=dogBoneDiameter, bulge=boneBulge)
-	else:
-		points, yUnit = genPoints(allPoints[-1], typeSide="thick", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=extraList[1], dogDia=dogBoneDiameter, bulge=boneBulge)
-	
-	points = rotate_points(points, 90, allPoints[-1])
-	allPoints = np.concatenate([allPoints, points])
+    if polarity > 0:
+        points, yUnit = genPoints(allPoints[-1], typeSide="thin", numFingers=numFingList[1], fingerLength=-fingerLength, clearence=clearence, span=spanList[1], extra=extraList[1], dogDia=dogBoneDiameter, bulge=boneBulge)
+    else:
+        points, yUnit = genPoints(allPoints[-1], typeSide="thick", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=extraList[1], dogDia=dogBoneDiameter, bulge=boneBulge)
 
-	rightPoint = points[-1]
+    points = rotate_points(points, 90, allPoints[-1])
+    allPoints = np.concatenate([allPoints, points])
 
-	finalStart = [orgin[0], allPoints[-1][1]]
+    rightPoint = points[-1]
 
-	if polarity > 0:
-		points, unit = genPoints(finalStart, typeSide="thin", numFingers=numFingList[1], fingerLength=-fingerLength, clearence=clearence, span=spanList[1], extra=fingerLength, dogDia=dogBoneDiameter, bulge=boneBulge)
-	else:
-		points, unit = genPoints(finalStart, typeSide="thick", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=fingerLength, dogDia=dogBoneDiameter, bulge=boneBulge)
-	
-	points = rotate_points(points, 270, finalStart)
-	allPoints = np.concatenate([allPoints, points])
+    finalStart = [orgin[0], allPoints[-1][1]]
 
-	if rotation == 0:
-		return(allPoints, [orgin, rightPoint], [xUnit, yUnit])
-	else:
-		return(rotate_points(allPoints, rotation, orgin), [orgin, rotate_points(np.array([rightPoint]), rotation, orgin)[0]], [xUnit, yUnit])
+    if polarity > 0:
+        points, unit = genPoints(finalStart, typeSide="thin", numFingers=numFingList[1], fingerLength=-fingerLength, clearence=clearence, span=spanList[1], extra=fingerLength, dogDia=dogBoneDiameter, bulge=boneBulge)
+    else:
+        points, unit = genPoints(finalStart, typeSide="thick", numFingers=numFingList[1], fingerLength=fingerLength, clearence=clearence, span=spanList[1], extra=fingerLength, dogDia=dogBoneDiameter, bulge=boneBulge)
+
+    points = rotate_points(points, 270, finalStart)
+    allPoints = np.concatenate([allPoints, points])
+
+    if rotation == 0:
+        return(allPoints, [orgin, rightPoint], [xUnit, yUnit])
+    else:
+        return(rotate_points(allPoints, rotation, orgin), [orgin, rotate_points(np.array([rightPoint]), rotation, orgin)[0]], [xUnit, yUnit])
+
 
 def create_dxf(allPoints, layerNames, filePrefix, fileName):
-	doc = ezdxf.new(dxfversion='R2010')  # Create a new DXF document
-	
-	for idx in range(len(layerNames)):
+    doc = ezdxf.new(dxfversion='R2010')  # Create a new DXF document
 
-		layer = doc.layers.new(name=layerNames[idx])  # Create Layer
-		msp = doc.modelspace()
-		msp.add_lwpolyline(allPoints[idx], format="xyb", dxfattribs={'layer': layer.dxf.name})
-	
-	doc.saveas(filePrefix + fileName + ".dxf")
+    for idx in range(len(layerNames)):
+
+        layer = doc.layers.new(name=layerNames[idx])  # Create Layer
+        msp = doc.modelspace()
+        msp.add_lwpolyline(allPoints[idx], format="xyb", dxfattribs={'layer': layer.dxf.name})
+
+    doc.saveas(filePrefix + fileName)
--- drawFingersLib.py
+++ drawFingersLib.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser, RawTextHelpFormatter
 import pretty_errors
 import sys
 from boxLib import *
@@ -19,43 +22,52 @@ cutClearence = 0.2
 # Send cut send recommends 0.01in (0.005in per side) of clearence
 
 
-spacing = fingerLength*.5
+spacing = fingerLength * 0.5
 
 baseLengthSpan = internalLength - cutClearence * 2.0
 baseWidthSpan = internalWidth - cutClearence * 2.0
 heightSpan = internalHeight - fingerLength
 
-filePrefix = "fingerBox"
-fileName = "_%sx%sx%s(%sx%sx%s)C%s_M%s_B%s" % (internalWidth, internalLength, internalHeight, numWidthFingers, numLengthFingers, numHeightFingers, cutClearence, materialThickness, dogBoneDia)
-print(fileName)
+if __name__ == "__main__":
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument(
+        "folder", metavar="folder", type=str, nargs="+", help="folder in which the DXF will be saved to")
+    parser.add_argument("-p", "--prefix", dest="prefix", type=str, required=False,
+                        help="DXF File Name Prefix", default="fingerBox")
+
+    args = parser.parse_args()
+    data_folder = args.folder
+    file_prefix = "".join(data_folder) + "\\" + args.prefix
+
+fileName = "_%sx%sx%s(%sx%sx%s)C%s_M%s_B%s.dxf" % (internalWidth, internalLength, internalHeight, numWidthFingers, numLengthFingers, numHeightFingers, cutClearence, materialThickness, dogBoneDia)
+print(file_prefix + fileName)
 
 # Generate Bottom
-ptLst1, boundryBot, units = gen_bottom(orgin=[0,0], numFingList=[numWidthFingers,numLengthFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseWidthSpan,baseLengthSpan], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
-print(units)
-create_dxf([ptLst1], ["bottom"], filePrefix + "A", fileName)
+ptLst1, boundryBot, units1 = gen_bottom(orgin=[0, 0], numFingList=[numWidthFingers, numLengthFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseWidthSpan, baseLengthSpan], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
+create_dxf([ptLst1], ["bottom"], file_prefix + "A", fileName)
 graphPoints(ptLst1)
 
 # Generate Right
-startPoint = [boundryBot[1][0] + spacing , boundryBot[1][1] + cutClearence + fingerLength]
-ptLst3, boundry, units = gen_Side(orgin=startPoint, rotation=270, numFingList=[numLengthFingers,-numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseLengthSpan,heightSpan], extraList=[cutClearence + fingerLength, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
-print(units)
-create_dxf([ptLst3], ["SideB"], filePrefix + "C", fileName)
+startPoint = [boundryBot[1][0] + spacing, boundryBot[1][1] + cutClearence + fingerLength]
+ptLst3, boundry, units2 = gen_Side(orgin=startPoint, rotation=270, numFingList=[numLengthFingers, -numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseLengthSpan, heightSpan], extraList=[cutClearence + fingerLength, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
+create_dxf([ptLst3], ["SideB"], file_prefix + "C", fileName)
 graphPoints(ptLst3, color="r")
 
 # Generate Top
 startPoint = [boundryBot[0][0] - cutClearence, boundryBot[1][1] + spacing]
-ptLst2, boundry, units = gen_Side(orgin=startPoint, rotation=0, numFingList=[numWidthFingers,numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseWidthSpan,heightSpan], extraList=[cutClearence, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
-print(units)
-create_dxf([ptLst2], ["SideA"], filePrefix + "B", fileName)
+ptLst2, boundry, units3 = gen_Side(orgin=startPoint, rotation=0, numFingList=[numWidthFingers, numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseWidthSpan, heightSpan], extraList=[cutClearence, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
+create_dxf([ptLst2], ["SideA"], file_prefix + "B", fileName)
 graphPoints(ptLst2, color="c")
 
-#Create dxf file of all shapes
-create_dxf([ptLst1, ptLst2, ptLst3], ["bot","S_T", "S_R"], filePrefix, fileName)
+# Create dxf file of all shapes
+create_dxf([ptLst1, ptLst2, ptLst3], ["bot", "S_T", "S_R"], file_prefix, fileName)
 
 # Generate Top Right (used to verify side heights match)
-startPoint = [boundry[1][0] + spacing , boundry[0][1]]
-ptLst3, boundry, units = gen_Side(orgin=startPoint, rotation=0, numFingList=[numLengthFingers,-numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseLengthSpan,heightSpan], extraList=[cutClearence + fingerLength, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
+startPoint = [boundry[1][0] + spacing, boundry[0][1]]
+ptLst3, boundry, units = gen_Side(orgin=startPoint, rotation=0, numFingList=[numLengthFingers, -numHeightFingers], fingerLength=fingerLength, clearence=cutClearence, spanList=[baseLengthSpan, heightSpan], extraList=[cutClearence + fingerLength, fingerLength], dogBoneDiameter=dogBoneDia, boneBulge=boneBulge)
 graphPoints(ptLst3, color="b")
 
+
+print("Width: %s\nLength: %s\nHeight: %s" % (np.around(units1, 2)[0],np.around(units1, 2)[1], np.around(units2, 2)[1]))
 plt.axis('scaled')
 plt.show()
