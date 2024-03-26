98117bb5d6a6eaf07973b58fa10ccff7b3b3ebc9
martinbros <67989270+martinbros@users.noreply.github.com>
2024-03-26 11:32:29 -0700

    Added capability to generate DXF files with appropriate dog-bone formation

--- generateFingers.py
+++ generateFingers.py
@@ -5,18 +5,15 @@ import numpy as np
 from boxLib import *
 
 class Edge:
-    def __init__(self, origin, numFingers, fingerLength, clearence, span, extra, dogBoneDia):
+    def __init__(self, origin, numFingers, fingerLength, clearence, span, extra):
         self.origin = origin
         self.numFingers = numFingers
         self.fingerLength = fingerLength
         self.clearence = clearence
         self.span = span
         self.extra = extra
-        self.dogBoneDia = dogBoneDia
         
         self.unit = span / (2.0 * numFingers + 1.0)  # Need to check if unit is less than dogBoneDia
-        #self.xList = np.empty((2 * numFingers + 1) * 2)
-        #self.yList = np.empty((2 * numFingers + 1) * 2)
     
     def genFingerPoints(self):
         self.xList = np.tile([self.unit - 2.0 * self.clearence, self.unit + self.clearence * 2.0], self.numFingers + 1)[:-1] # Create a tile of finger/gap widths
@@ -29,17 +26,48 @@ class Edge:
         
         self.yList = np.tile([self.origin[1], self.origin[1], self.origin[1] + self.fingerLength, self.origin[1] + self.fingerLength], self.numFingers + 1)[:-2]
         
-    def genFingerPointsBone(self):
-        self.dogBoneOffsetX = 1.0
-        self.botHorz = self.unit - 2.0 * self.clearence - 2.0 * self.dogBoneOffsetX
-        self.topHorz = self.unit + 2.0 * self.clearence - 2.0 * self.dogBoneOffsetX
-                
-        self.dogBoneOffsetY = 1.0
+    def genFingerPointsBone(self, dogBoneDia=0.0, dogBoneType=None, invertBone=False):
+        
+        if dogBoneType == "H":
+            self.dogBoneOffsetX = dogBoneDia
+            self.dogBoneOffsetY = 0.0
+        
+        elif dogBoneType == "I":
+            self.dogBoneOffsetX = 0.0
+            self.dogBoneOffsetY = dogBoneDia * (self.fingerLength / np.abs(self.fingerLength))
+        
+        elif dogBoneType == "X":
+            self.dogBoneOffsetX = dogBoneDia
+            self.dogBoneOffsetY = dogBoneDia * (self.fingerLength / np.abs(self.fingerLength))
+        
+        else:
+            self.dogBoneOffsetX = 0.0
+            self.dogBoneOffsetY = 0.0
+                       
         self.botVert = self.origin[1] + self.dogBoneOffsetY
         self.topVert = self.origin[1] + self.fingerLength - self.dogBoneOffsetY
         self.fingerOut = self.origin[1] + self.fingerLength
         
-        self.xList = np.tile([self.dogBoneOffsetX, self.botHorz, self.dogBoneOffsetX, self.dogBoneOffsetX, self.topHorz, self.dogBoneOffsetX], self.numFingers + 1)[:-3]
+        if invertBone:  # dogbone is on the bottom
+            self.bulgeList = np.tile([1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0], (self.numFingers + 1))[:-4] * (self.fingerLength / np.abs(self.fingerLength))
+            self.bulgeList[0] = 0.0
+            self.bulgeList[-2] = 0.0
+            
+            self.botHorz = self.unit - 2.0 * self.clearence - 2.0 * self.dogBoneOffsetX
+            self.topHorz = self.unit + 2.0 * self.clearence
+            
+            elementWidths = [self.dogBoneOffsetX, self.botHorz, self.dogBoneOffsetX, 0.0, self.topHorz, 0.0]
+        
+        else:  # Dogbone is on the top
+            self.bulgeList = np.tile([0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0], (self.numFingers + 1))[:-4] * -1 * (self.fingerLength / np.abs(self.fingerLength))
+            
+            self.botHorz = self.unit - 2.0 * self.clearence
+            self.topHorz = self.unit + 2.0 * self.clearence - 2.0 * self.dogBoneOffsetX
+            
+            elementWidths = [0.0, self.botHorz, 0.0, self.dogBoneOffsetX, self.topHorz, self.dogBoneOffsetX]
+                
+             
+        self.xList = np.tile(elementWidths, self.numFingers + 1)[:-3]
         self.xList[1] = self.xList[1] + self.clearence + self.extra # For the first and last horizontal, add on clearence and extra
         self.xList[-2] = self.xList[-2] + self.clearence + self.extra
         self.xList = np.cumsum(self.xList)  # Generate all of the x-points from the widths
@@ -55,15 +83,64 @@ class Edge:
         self.yList[0] = self.origin[1]
         self.yList[-1] = self.origin[-1]
         
+        self.fingerPoints = np.dstack((self.xList, self.yList, self.bulgeList))[0]
+        
         
     
-edgeA = Edge([0.0, 2.0], 3, 10.0, 0.5, 28.0, 0.0, 1.0)
-edgeA.genFingerPointsBone()
+edgeHT = Edge([0.0, 1.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeHT.genFingerPointsBone(1.0, "H", True)
 
-edgeB = Edge([0.0, 0.0], edgeA.numFingers, 10.0, 0.0, edgeA.span, 0.0, 1.0)
-edgeB.genFingerPoints()
+edgeHF = Edge([0.0, 2.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeHF.genFingerPointsBone(1.0, "H", False)
+
+edgeIT = Edge([0.0, 3.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeIT.genFingerPointsBone(1.0, "I", True)
+
+edgeIF = Edge([0.0, 4.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeIF.genFingerPointsBone(1.0, "I", False)
+
+edgeXT = Edge([0.0, 5.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeXT.genFingerPointsBone(1.0, "X", True)
+
+edgeXF = Edge([0.0, 6.0], 3, -10.0, 0.5, 28.0, 0.0)
+edgeXF.genFingerPointsBone(1.0, "X", False)
+
+edgeA = Edge([0.0, 0.0], 3, -10.0, 0.0, 28.0, 0.0)
+edgeA.genFingerPoints()
 
 
 plt.plot(edgeA.xList, edgeA.yList, color="k", marker="o")
-plt.plot(edgeB.xList, edgeB.yList, color="g", marker="o")
+
+plt.plot(edgeHT.xList, edgeHT.yList, color="b", marker="o")
+plt.plot(edgeHF.xList, edgeHF.yList, color="g", marker="o")
+
+plt.plot(edgeIT.xList, edgeIT.yList, color="r", marker="o")
+plt.plot(edgeIF.xList, edgeIF.yList, color="c", marker="o")
+
+plt.plot(edgeXT.xList, edgeXT.yList, color="m", marker="o")
+plt.plot(edgeXF.xList, edgeXF.yList, color="violet", marker="o")
+
 plt.axis('scaled')
+
+doc = ezdxf.new(dxfversion='R2010')  # Create a new DXF document
+msp = doc.modelspace()
+
+layerHT = doc.layers.new(name="HT")  # Create Layer
+msp.add_lwpolyline(edgeHT.fingerPoints, format="xyb", dxfattribs={'layer': layerHT.dxf.name})
+
+layerHF = doc.layers.new(name="HF")  # Create Layer
+msp.add_lwpolyline(edgeHF.fingerPoints, format="xyb", dxfattribs={'layer': layerHF.dxf.name})
+
+layerIT = doc.layers.new(name="IT")  # Create Layer
+msp.add_lwpolyline(edgeIT.fingerPoints, format="xyb", dxfattribs={'layer': layerIT.dxf.name})
+
+layerIF = doc.layers.new(name="IF")  # Create Layer
+msp.add_lwpolyline(edgeIF.fingerPoints, format="xyb", dxfattribs={'layer': layerIF.dxf.name})
+
+layerXT = doc.layers.new(name="XT")  # Create Layer
+msp.add_lwpolyline(edgeXT.fingerPoints, format="xyb", dxfattribs={'layer': layerXT.dxf.name})
+
+layerXF = doc.layers.new(name="XF")  # Create Layer
+msp.add_lwpolyline(edgeXF.fingerPoints, format="xyb", dxfattribs={'layer': layerXF.dxf.name})
+
+doc.saveas("testFile.dxf")
