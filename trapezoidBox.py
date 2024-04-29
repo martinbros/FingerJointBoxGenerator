7181b28b6f31b920f46346ef2d5c64125e00625e
martinbros <67989270+martinbros@users.noreply.github.com>
2024-04-29 00:34:01 -0700

    Added in drill points, export to multiple dxf files, added in user argument to create directory

--- trapezoidBox.py
+++ trapezoidBox.py
@@ -5,17 +5,21 @@ from generateFingers import Edge, plotLinePoints, dxfFromDict
 import matplotlib.pyplot as plt
 import numpy as np
 import ezdxf
+import os
+import shutil
+import sys
 
 boxInnerFootprint = [600.0, 175.0]
 trapBase = 600.0
 boxHeight = 200.0
+drillRad = 1.0
 
 materialThickness = 10.0
 materialThicknessBase = 20.0
 dogBoneDia = 3.175 + 0.5
 dogBoneType = "I"
-#clearence = 0.2 / 2.0
-clearence = 0.0
+clearence = 0.2 / 2.0
+#clearence = 0.0
 
 # Dimensions for the bearing race
 marbleDia = 10.0
@@ -45,7 +49,20 @@ lgAngFingLen = materialThickness * (np.tan(np.pi / 2.0 - angle) + 1 / np.cos(np.
 smAngOff = materialThickness / np.tan(angle)  # Smaller angle, backoff from vertex
 lgAngOff = materialThickness / np.cos(np.pi / 2.0 - angle)  # Larger angle, backoff from vertex THIS ONE SHOULD BE 0 when angle is 90deg
 
-layers = {}
+saveFolder = sys.argv[1]
+
+if not os.path.exists(saveFolder):
+	os.makedirs(saveFolder)
+else:
+	for filename in os.listdir(saveFolder):
+		file_path = os.path.join(saveFolder, filename)
+		try:
+			if os.path.isfile(file_path) or os.path.islink(file_path):
+				os.unlink(file_path)
+			elif os.path.isdir(file_path):
+				shutil.rmtree(file_path)
+		except Exception as e:
+			print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 ##################
 #
@@ -55,11 +72,11 @@ layers = {}
 
 wallOrigin = [boxInnerFootprint[1] / -2.0, 0.0]
 tEdge = Edge(2, -materialThickness, clearence, boxInnerFootprint[1] - 2.0 * lgAngOff, lgAngOff)
-tEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False)
+tEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False, drillNum = 1)
 tEdge.rotateShiftElement("finger", wallOrigin)
 
 rEdge = Edge(2, -materialThickness, clearence, hypotSpan, 0.0)
-rEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False)
+rEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False, drillNum = 1)
 rEdge.rotateShiftElement("finger", tEdge.cordsFinger[-1], -angleDeg)
 
 bEdge = Edge(4, materialThicknessBase, -clearence, trapBase, 0.0)
@@ -67,14 +84,23 @@ bEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True)
 bEdge.rotateShiftElement("finger", rEdge.cordsFinger[-1], 180.0)
 
 lEdge = Edge(rEdge.numFingers, -materialThickness, clearence, rEdge.span, rEdge.extra)
-lEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False)
+lEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False, drillNum = 1)
 lEdge.rotateShiftElement("finger", bEdge.cordsFinger[-1], angleDeg)
 
 trapWall = np.concatenate([tEdge.cordsFinger[:-1], rEdge.cordsFinger[:-1], bEdge.cordsFinger[:-1], lEdge.cordsFinger])
 trapWall[-1] = [wallOrigin[0], wallOrigin[1], 0.0]
 
+layers = {}
 layers["trap"] = trapWall
-#plotLinePoints(trapWall, "line", color="k", marker="o")
+
+drillList = np.concatenate([tEdge.cordsDrill, rEdge.cordsDrill, lEdge.cordsDrill])
+drillRadList = np.full([drillList.shape[0], 1], drillRad)
+drillPoints = {}
+drillPoints["drill"] = np.append(drillList, drillRadList, axis=1)
+
+dxfFromDict(layers, saveFolder + "\\trapezoid.dxf", drillPoints)
+#plotLinePoints(layers["trap"], "line", color="k", marker="o")
+#plotLinePoints(drillPoints["drill"], "circle", color="k")
 
 ##################
 #
@@ -84,7 +110,7 @@ layers["trap"] = trapWall
 
 wallOrigin = [0.0, 0.0]
 shortEdge = Edge(tEdge.numFingers, -materialThickness, -clearence, tEdge.span, lgAngFingLen)
-shortEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True)
+shortEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True, drillNum=1)
 shortEdge.rotateShiftElement("finger", wallOrigin)
 
 longEdge = Edge(4, lgAngFingLen, clearence, boxInnerFootprint[0] - materialThickness * 2.0, 0.0)
@@ -92,7 +118,7 @@ longEdge.genFingerPointsBone(dogBoneDia, dogBoneType, False)
 longEdge.rotateShiftElement("finger", shortEdge.cordsFinger[-1], 90.0)
 
 shortEdgeTop = Edge(shortEdge.numFingers, shortEdge.fingerLength, shortEdge.clearence, shortEdge.span, shortEdge.extra)
-shortEdgeTop.genFingerPointsBone(dogBoneDia, dogBoneType, True)
+shortEdgeTop.genFingerPointsBone(dogBoneDia, dogBoneType, True, drillNum=1)
 shortEdgeTop.rotateShiftElement("finger", longEdge.cordsFinger[-1], 180.0)
 
 longEdgeLeft = Edge(longEdge.numFingers, longEdge.fingerLength, longEdge.clearence, longEdge.span, longEdge.extra)
@@ -102,8 +128,17 @@ longEdgeLeft.rotateShiftElement("finger", shortEdgeTop.cordsFinger[-1], 270.0)
 platWall = np.concatenate([shortEdge.cordsFinger[:-1], longEdge.cordsFinger[:-1], shortEdgeTop.cordsFinger[:-1], longEdgeLeft.cordsFinger])
 platWall[-1] = [wallOrigin[0], wallOrigin[1], 0.0]
 
+layers = {}
 layers["top"] = platWall
+
+drillList = np.concatenate([shortEdge.cordsDrill, shortEdgeTop.cordsDrill])
+drillRadList = np.full([drillList.shape[0], 1], drillRad)
+drillPoints = {}
+drillPoints["drill"] = np.append(drillList, drillRadList, axis=1)
+
+dxfFromDict(layers, saveFolder + "\\top.dxf", drillPoints)
 #plotLinePoints(platWall, "line", color="c", marker="o")
+#plotLinePoints(drillPoints["drill"], "circle", color="k")
 
 ##################
 #
@@ -113,7 +148,7 @@ layers["top"] = platWall
 
 wallOrigin = [0.0, materialThickness]
 angleEdge = Edge(rEdge.numFingers, -materialThickness, -clearence, hypotSpan, [-smAngOff, -lgAngOff])
-angleEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True)
+angleEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True, drillNum=1)
 angleEdge.rotateShiftElement("finger", wallOrigin)
 
 baseEdge = Edge(4, -lgAngFingLen, -clearence, longEdge.span, 0.0)
@@ -121,7 +156,7 @@ baseEdge.genFingerPointsBone(dogBoneDia, dogBoneType, True)
 baseEdge.rotateShiftElement("finger", angleEdge.cordsFinger[-1], 90.0)
 
 angleEdgeTop = Edge(rEdge.numFingers, -materialThickness, -clearence, angleEdge.span, [-lgAngOff, -smAngOff])
-angleEdgeTop.genFingerPointsBone(dogBoneDia, dogBoneType, True)
+angleEdgeTop.genFingerPointsBone(dogBoneDia, dogBoneType, True, drillNum=1)
 angleEdgeTop.rotateShiftElement("finger", baseEdge.cordsFinger[-1], 180.0)
 
 topEdge = Edge(baseEdge.numFingers, -smAngFingLen, -clearence, baseEdge.span, 0.0)
@@ -131,8 +166,17 @@ topEdge.rotateShiftElement("finger", angleEdgeTop.cordsFinger[-1], 270.0)
 angledWall = np.concatenate([angleEdge.cordsFinger[:-1], baseEdge.cordsFinger[:-1], angleEdgeTop.cordsFinger[:-1], topEdge.cordsFinger])
 angledWall[-1] = [wallOrigin[0], wallOrigin[1], 0.0]
 
-layers["angled"] = angledWall 
+layers = {}
+layers["angled"] = angledWall
+
+drillList = np.concatenate([angleEdge.cordsDrill, angleEdgeTop.cordsDrill])
+drillRadList = np.full([drillList.shape[0], 1], drillRad)
+drillPoints = {}
+drillPoints["drill"] = np.append(drillList, drillRadList, axis=1)
+
+dxfFromDict(layers, saveFolder + "\\angled.dxf", drillPoints)
 #plotLinePoints(angledWall, "line", color="k", marker="o")
+#plotLinePoints(drillPoints["drill"], "circle", color="k")
 
 ##################
 #
@@ -141,15 +185,16 @@ layers["angled"] = angledWall
 ##################
 
 # trapezoid wall mate holes
-bEdge.genHoleBone(materialThickness + clearence * 2.0, clearence, dogBoneDia, "H")
+bEdge.genHoleBone(materialThickness + clearence * 2.0, clearence, dogBoneDia, "H", drillNum=1)
 bEdge.rotateShiftElement("hole", [0.0, materialThickness / 2.0])
+drillList = np.array(bEdge.cordsHolesDrill)
 
 holeWidth = materialThickness * np.cos(np.pi / 2.0 - angle) + np.abs(topEdge.fingerLength) * np.cos(angle) + clearence * 2.0
 holeOffsetFromEdge = materialThickness / np.sin(angle) + clearence
 holeCenterLine = trapBase - holeOffsetFromEdge + (holeWidth / 2.0)
 
 # angled wall mate holes
-baseEdge.genHoleBone(holeWidth, clearence, dogBoneDia, "H")
+baseEdge.genHoleBone(holeWidth, clearence, dogBoneDia, "H", drillNum=1)
 baseEdge.rotateShiftElement("hole", [holeCenterLine, materialThickness], 90.0)
 
 for hole in baseEdge.cordsHoles:
@@ -158,6 +203,7 @@ for hole in baseEdge.cordsHoles:
 mountHoles = bEdge.cordsHoles
 
 bEdge.rotateShiftElement("hole", [bEdge.span, baseEdge.span + materialThickness * 2.0], 180.0)
+drillList = np.concatenate([drillList, bEdge.cordsHolesDrill])
 
 for hole in bEdge.cordsHoles:
 	mountHoles.append(hole)
@@ -190,26 +236,37 @@ circleArmPoints = shift.rotateAndShift(circleArmPoints, shiftOrigin=[trapBase / 
 
 mountHoles.append(circleArmPoints)
 
+layers = {}
 layers["base"] = mountHoles
+
+drillPoints = {}
+drillPoints["race"] = np.array([[trapBase / 2.0, boxInnerFootprint[0] / 2.0, raceRad]])
+
+drillRadList = np.full([drillList.shape[0], 1], drillRad)
+drillPoints["drill"] = np.append(drillList, drillRadList, axis=1)
+
+dxfFromDict(layers, saveFolder + "\\base.dxf", drillPoints)
 plotLinePoints(mountHoles, "line", color="r")
+plotLinePoints(drillPoints["drill"], "circle", color="k")
 
 ##################
 #
 #  Generate Foot
 #
 ##################
-
+layers = {}
 layers["foot"] = outline
 
 drillPoints = {}
-drillPoints["foot"] = np.array([[trapBase / 2.0, boxInnerFootprint[0] / 2.0, raceRad], [trapBase / 2.0, boxInnerFootprint[0] / 2.0, 30.0]])
-drillPoints["base"] = np.array([[trapBase / 2.0, boxInnerFootprint[0] / 2.0, raceRad]])
+drillPoints["foot"] = np.array([[trapBase / 2.0, boxInnerFootprint[0] / 2.0, 30.0]])
+drillPoints["race"] = np.array([[trapBase / 2.0, boxInnerFootprint[0] / 2.0, raceRad]])
 
+dxfFromDict(layers, saveFolder + "\\foot.dxf", drillPoints)
 #plotLinePoints(outline, "line", color="b")
 
 ##################
 #
-#  Generate race
+#  Generate cage
 #
 ##################
 
@@ -224,18 +281,20 @@ for num in range(marbleNum):
 bearingHoles.append([0, 0, raceRad + (marbleDia / 2.0 + marbleBuffer + raceBuffer)])
 bearingHoles.append([0, 0, raceRad - (marbleDia / 2.0 + marbleBuffer + raceBuffer)])
 bearingHoles = shift.rotateAndShift(np.array(bearingHoles), shiftOrigin=[trapBase / 2.0, boxInnerFootprint[0] / 2.0])
+
+drillPoints = {}
 drillPoints["race"] = bearingHoles
 
-plotLinePoints(np.array(bearingHoles), "circle", color="k")
+dxfFromDict({}, saveFolder + "\\cage.dxf", drillPoints)
+#plotLinePoints(np.array(bearingHoles), "circle", color="k")
 
 
 ##################
 #
-#  Generate DXF
+#  plot parameters
 #
 ##################
 
-dxfFromDict(layers, "trapezoidBoxBaseThick.dxf", drillPoints)
 plt.axis('scaled')
 plt.tight_layout()
 plt.show()
